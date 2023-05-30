fe38613ec24d0c386f5d182f1697ca7677b7feba
Juan Navarro <juansnn@pm.me>
2023-05-30 21:17:52 +0000

    main function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,49 @@
-@main def hello: Unit =
-  println(puduImg)
+import scala.collection.mutable.HashMap
 
-/* https://www.asciiart.eu/animals/deer */
-val puduImg = """
-         /',  _
-       _(  ;-'.'
-  _,-'~     '".
-'"          ~. .
-        _.      '. 
-    _.-'  ~'--.   ) 
-  '~           ~--'=._
-               /)_.-.
-           _.-' ' <~
-        .-"    _ ~ \
-            .-' '-._)
-          .'                     PjP"""
+import scala.io.StdIn
+import scala.util.{Try, Success, Failure}
 
+import puducalc._
+
+import pudu.parser.generator._
+
+@main def main: Unit =
+  val parser = SLRParserGenerator(ParserSpec).parser
+  val strParser = parser.compose(Lexer.lexer)
+
+  val ctx = HashMap[String, Double]()
+
+  val analize = TreeAnalizer(ctx).analize
+  val evaluate = TreeEvaluator(ctx).eval
+
+  val quitStr = ".quit"
+  var lineNo = 0
+
+  val mainIterator = Iterator.continually(StdIn.readLine())
+    .takeWhile(_ != quitStr)
+    .map(strParser(_))
+    .tapEach {
+      case Left(err) => println(s"Error: ${err.msg}")
+      case _ => () }
+    .collect { case Right(tree) => tree }
+    .map {
+      case t @ Assignment(id, expr) => (t, analize(expr))
+      case t: ExprTree => (t, analize(t))
+      case _: ExprSeq => throw Exception() }
+    .tapEach { case (_, undef) =>
+      if ! undef.isEmpty then
+        println(s"Undefined variables or functions: $undef") }
+    .filter { case (_, undef) => undef.isEmpty }
+    .map { _._1 }
+
+  for tree <- mainIterator do
+    tree match
+      case Assignment(v, expr) =>
+        val res = evaluate(expr)
+        ctx += v -> res
+        println(s"Assigned '$v' := $res")
+      case t: ExprTree =>
+        val res = evaluate(t)
+        lineNo += 1
+        println(s"Result($lineNo) is $res")
+      case _ => ()
--- src/main/scala/puducalc/TreeAnalizer.scala
+++ src/main/scala/puducalc/TreeAnalizer.scala
@@ -1,24 +1,41 @@
 package puducalc
 
+/** Sets of undefined variables and functions */
 case class UndefinedIds(vars: Set[String], functions: Set[String]):
   def addVar(v: String) = UndefinedIds(vars + v, functions)
   def addFn(fn: String) = UndefinedIds(vars, functions + fn)
+
   def ++(that: UndefinedIds) = UndefinedIds(vars ++ that.vars, functions ++ that.functions)
+  def isEmpty = vars.isEmpty && functions.isEmpty
+
+  override def toString =
+    def setToString(name: String, set: Set[String]) =
+      require(!set.isEmpty)
+      val setStr = set.mkString(",")
+      s"$name: <$setStr>"
+    def varsStr = setToString("variables", vars)
+    def funcStr = setToString("functions", functions)
+    if isEmpty then ""
+    else if vars.isEmpty then funcStr
+    else if functions.isEmpty then varsStr
+    else s"$varsStr - $funcStr"
 
 object UndefinedIds:
   def empty = UndefinedIds(Set.empty, Set.empty)
 
+/** Collects all undefined variables and functions */
 class TreeAnalizer(ctx: scala.collection.Map[String, Double]):
   val functions = Set("sum", "min", "max", "avg")
   def analize: ExprTree => UndefinedIds =
     case ConstDouble(d) => UndefinedIds.empty
-    case Var(id) => if ! ctx.contains(id) then UndefinedIds.empty.addVar(id) else UndefinedIds.empty
+    case Var(id) => if ! ctx.contains(id) then UndefinedIds(Set(id), Set.empty)
+                    else UndefinedIds.empty
     case Addition(l, r) => analize(l) ++ analize(r)
     case Subtraction(l, r) => analize(l) ++ analize(r)
     case Multiplication(l, r) => analize(l) ++ analize(r)
     case Division(l, r) => analize(l) ++ analize(r)
     case Pow(l, r) => analize(l) ++ analize(r)
     case UMinus(expr) => analize(expr)
-    case FuncCall(fn, args) => 
+    case FuncCall(fn, args) =>
       val argsUndefIds = args.map(analize).reduce(_ ++ _)
       if !functions.contains(fn) then argsUndefIds.addFn(fn) else argsUndefIds
